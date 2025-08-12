7e7843ea1798a68b2447ebb05ef33f413557b2d2
Melo3005 <murilomelo3041@gmail.com>
2025-08-12 20:46:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let listaGenrerica = [];
+let linguagensDeProgramcao = ['Java', 'Python', 'JavaScript'];
+console.log(listaGenrerica.push['Java', 'Python', 'JavaScript'])
+
+nomes = ['Java', 'Python', 'JavaScript']
+console.log(nomes[0])
+
+nomes = ['Java', 'Python', 'JavaScript']
+console.log(nomes[1])
+
+nomes = ['Java', 'Python', 'JavaScript']
+console.log(nomes[2])
